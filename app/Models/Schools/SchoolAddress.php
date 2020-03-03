899fcabcf842b1f6173209ffcfb152c7d5c6ba70
grcat <b6024636@my.shu.ac.uk>
2020-03-03 13:54:21 +0000

    ...

--- app/Models/Schools/SchoolAddress.php
+++ app/Models/Schools/SchoolAddress.php
@@ -1,11 +1,22 @@
 <?php
 
-namespace App;
+namespace App\Models\Schools;
 
 use Illuminate\Database\Eloquent\Model;
+use Eloquent;
 
+/**
+ * Post
+ *
+ * @mixin Eloquent
+ */
 class SchoolAddress extends Model
 {
+    public function school()
+    {
+        return $this->belongsTo('App\Models\Schools\School');
+    }
+
     protected $fillable = [
         'Address1',
         'Address2',
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <div class="title m-b-md">Schools</div>
     <div class="links">
-        <a href="{{ config('app.url')}}/schooltest/create">Create School</a>
-        <a href="{{ config('app.url')}}/schooltest">View School</a>
+        <a href="{{ config('app.url')}}/schools/school/create">Create School</a>
+        <a href="{{ config('app.url')}}/schools/school">View Schools</a>
     </div>
 @endsection
